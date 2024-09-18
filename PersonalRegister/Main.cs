f3ee28c16e41f8875b0f0db511e1b4c44bf3149f
Jonathan Krall <jonathan.krall@lexicon.se>
2024-09-18 09:54:15 +0200

    Moved code away from Program

---
+++ PersonalRegister/Main.cs
@@ -0,0 +1,71 @@
+﻿using PersonalRegister.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister
+{
+    internal class Main
+    {
+        private static Payroll payroll = new Payroll();
+
+        public void Run()
+        {
+            bool isAlive = true;
+            SeedData();
+            do
+            {
+
+                MenuHelpers.ShowMainMenu();
+
+                string input = Console.ReadLine().ToUpper();
+
+                switch (input)
+                {
+                    case MenuHelpers.Add:
+                        AddEmployee();
+                        break;
+                    case MenuHelpers.Print:
+                        PrintEmployees();
+                        break;
+                    case MenuHelpers.Quit:
+                        isAlive = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (isAlive);
+        }
+
+        private static void AddEmployee()
+        {
+            string name = Util.AskForString("Name");
+            uint salary = Util.AskForUInt("Salary");
+
+            //Employee employee = new Employee(name, salary);
+
+            //payroll.AddEmployee(name, salary);
+            payroll.AddEmployee(new Employee(name, salary));
+        }
+        private static void PrintEmployees()
+        {
+            IEnumerable<Employee> employees = payroll.GetEmployees();
+
+            foreach (Employee employee in employees)
+            {
+                //Console.WriteLine($"Name: {employee.Name} Salary: {employee.Salary}");
+                Console.WriteLine(employee);
+            }
+        }
+
+        private static void SeedData()
+        {
+            payroll.AddEmployee("Kalle", 25000);
+            payroll.AddEmployee("Anna", 28000);
+            payroll.AddEmployee("Stefan", 35000);
+        }
+    }
+}
--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -5,62 +5,14 @@ namespace PersonalRegister
 {
     internal class Program
     {
-        private static Payroll payroll = new Payroll(); 
+       
         static void Main(string[] args)
         {
-            bool isAlive = true;
-            SeedData(); 
-            do
-            {
+            Main main = new Main();
 
-                MenuHelpers.ShowMainMenu();
+            main.Run(); 
+        }     
 
-                string input = Console.ReadLine().ToUpper();
-
-                switch (input)
-                {
-                    case MenuHelpers.Add:
-                        AddEmployee();
-                        break;
-                    case MenuHelpers.Print:
-                        PrintEmployees();
-                        break;
-                    case MenuHelpers.Quit:
-                        isAlive = false; 
-                        break;
-                    default:
-                        break;
-                }
-
-            } while (isAlive);
-        }       
-
-        private static void AddEmployee()
-        {
-            string name = Util.AskForString("Name");
-            uint salary = Util.AskForUInt("Salary");
-
-            //Employee employee = new Employee(name, salary);
-
-            //payroll.AddEmployee(name, salary);
-            payroll.AddEmployee(new Employee(name, salary));
-        }
-        private static void PrintEmployees()
-        {
-            IEnumerable<Employee> employees = payroll.GetEmployees();
-
-            foreach(Employee employee in employees)
-            {
-                //Console.WriteLine($"Name: {employee.Name} Salary: {employee.Salary}");
-                Console.WriteLine(employee); 
-            }
-        }
-
-        private static void SeedData()
-        {
-            payroll.AddEmployee("Kalle", 25000);
-            payroll.AddEmployee("Anna", 28000);
-            payroll.AddEmployee("Stefan", 35000);
-        }
+       
     }
 }
